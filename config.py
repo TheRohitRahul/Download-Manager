98f3ec0db0fd2236975f9d5ca07bf3636aa3fb4f
Rohit Rahul <therohitrahul@gmail.com>
2020-09-25 17:07:06 +0530

    added more extensions

--- config.py
+++ config.py
@@ -7,16 +7,23 @@ FOLDER_TO_ORGANIZE = "Downloads"
 ################## ADD ALL FILES TO CONSIDER HERE ##################################
 
 FILES_TO_CONSIDER = {
-						"VIDEO" : [".mp4",".mkv"],
+						"VIDEO" : [".mp4",".mkv", ".webm"],
 						"AUDIO" : [".mp3"],
-						"DOCUMENTS" : [".pdf",".ods"],
+						"DOCUMENTS" : [".pdf", ".doc", ".docx", "PDF", ".odt"],
+						"HTMLFILES" :[".html"],
+						"PRESENTATIONS" : [".pptx", ".ppt", ".odp"],
+						"JSONFILES":[".json"],
+						"TEXTFILES" : [".txt"],
+						"ExcelSheets" : [".xlsx", ".xls",".ods"],
+						"certificates" : [".crt"],
 						"DATASETS" : [".csv"],
-						"COMPRESSED" : [".zip",".gz",".rar",".tgz",".bz2"],
-						"IMAGES" : [".jpg",".png",".JPG"],
+						"COMPRESSED" : [".zip",".gz",".rar",".tgz",".bz2", ".tar", ".7z"],
+						"IMAGES" : [".jpg",".png",".JPG", ".jpeg", ".tif"],
+						"SQLFILES" : [".sql"],
 						"JAVA_LIBRARY_FILES" : [".jar"],
 						"PYTHON_PROGRAMS" : [".py"],
 						"JAVASCRIPT_PROGRAMS" : [".js"],
-						"INSTALLERS" : [".deb",".exe",".run"],
+						"INSTALLERS" : [".deb",".exe",".run", ".AppImage"],
 						"TORRENT" : [".torrent"],
 						"E_BOOKS" : [".epub",".mobi"],
 						"DISC_IMAGES" : [".iso"]
